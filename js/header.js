1a344fecb9049a1d3ac7c35a22b82f079c2fef00
CaioN <1808837298@qq.com>
2020-02-23 03:26:26 +0800

    Site updated: 2020-02-23 03:26:24

--- js/header.js
+++ js/header.js
@@ -207,18 +207,4 @@ $(document).ready(function () {
   // Initializaiton
   Stun.utils.pjaxReloadHeader();
   Stun.utils.pjaxReloadScrollIcon();
-  //百度提交
-  // var bp = document.createElement('script');
-  // var curProtocol = window.location.protocol.split(':')[0];
-  // if (curProtocol === 'https') {
-  //   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-  // }
-  // else {
-  //   bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-  // }
-  // var s = document.getElementsByTagName("script")[0];
-  // s.parentNode.insertBefore(bp, s);
-  //360提交
-  //var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-  //document.write('<script src="' + src + '" id="sozz"><\/script>');
 });
