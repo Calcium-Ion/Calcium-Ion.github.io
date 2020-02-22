2cc874b575d786454831f46826378d27ab80a1ec
CaioN <1808837298@qq.com>
2020-02-23 01:39:34 +0800

    Site updated: 2020-02-23 01:39:33

--- js/header.js
+++ js/header.js
@@ -219,6 +219,6 @@ $(document).ready(function () {
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(bp, s);
   //360提交
-  var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-  document.write('<script src="' + src + '" id="sozz"><\/script>');
+  //var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+  //document.write('<script src="' + src + '" id="sozz"><\/script>');
 });
