372003845d2b78e6cba4f2aca89abed076146c6c
CaioN <1808837298@qq.com>
2020-02-23 21:26:54 +0800

    Site updated: 2020-02-23 21:26:53

--- js/header.js
+++ js/header.js
@@ -207,4 +207,15 @@ $(document).ready(function () {
   // Initializaiton
   Stun.utils.pjaxReloadHeader();
   Stun.utils.pjaxReloadScrollIcon();
+
+  var bp = document.createElement('script');
+  var curProtocol = window.location.protocol.split(':')[0];
+  if (curProtocol === 'https') {
+    bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else {
+    bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(bp, s);
 });
